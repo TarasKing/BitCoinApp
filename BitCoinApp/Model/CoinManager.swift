2ddba41cb685572e5009a364e17c907df66e814a
TarasKing <ivanovtaras@live.ru>
2023-03-07 12:27:42 -0800

    Initial Commit

---
+++ BitCoinApp/Model/CoinManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoinManager.swift
+//  BitCoinApp
+//
+//  Created by Taras on 06/03/2023.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "API_KEY"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
